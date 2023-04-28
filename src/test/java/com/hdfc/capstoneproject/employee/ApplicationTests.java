3d011d8b1a5690906ff5bc612062a8c8cdaf20cd
hdfc-repo-adars <adarshverma8u@gmail.com>
2023-04-28 10:16:45 +0530

    jenkin to docker

---
+++ src/test/java/com/hdfc/capstoneproject/employee/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hdfc.capstoneproject.employee;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Disabled
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
